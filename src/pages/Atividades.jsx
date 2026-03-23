449cbb410fce7cf907ab9ac9201f0181b1acf38a
thamyris conceição <thamyrisconceicao334@gmail.com>
2026-03-23 19:53:06 +0000

    File changes

--- src/pages/Atividades.jsx
+++ src/pages/Atividades.jsx
@@ -6,7 +6,8 @@ import TeamScoreboard from "@/components/atividades/TeamScoreboard";
 import TeamActivityChart from "@/components/atividades/TeamActivityChart";
 import SellerCard from "@/components/atividades/SellerCard";
 import SellerProfilePage from "@/components/atividades/SellerProfilePage";
-import { subDays } from "date-fns";
+import { subDays, startOfDay, endOfDay } from "date-fns";
+import { utcToZonedTime, zonedTimeToUtc } from "date-fns-tz";
 import { getCategory } from "@/lib/eventUtils";
 
 // Retorna início/fim do dia em SP em timestamps UTC
